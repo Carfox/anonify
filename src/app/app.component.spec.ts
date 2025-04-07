2337318780d6d8ea5f4de60a011b47c2e2158b3a
Carfox <carlotreball@gmail.com>
2025-04-07 15:54:51 -0500

    update: rename in configuration files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'Anonyfy' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tesis-frontend');
+    expect(app.title).toEqual('anonify');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tesis-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, anonify');
   });
 });
